35aea25893832bc80ae8fedaf66a6671ac317564
Minwoo Jeong <jeongmwj@gmail.com>
2025-08-03 09:12:57 +0900

    feat(rust): add Rust development plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,82 @@ return {
     end,
   },
 
+  -- Rust development
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^6', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    ft = "rust",
+    config = function ()
+      local codelldb_pkg = vim.fn.expand("$MASON/packages/codelldb")
+      local extension_path = codelldb_pkg .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
+      local cfg = require('rustaceanvim.config')
+
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end
+  },
+
+  -- DAP, Debugger Adapter Protocol
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end
+  },
+
+  -- DAP UI
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+      require("dapui").setup()
+    end,
+  },
+
+  -- Rust Formatting
+  {
+    'rust-lang/rust.vim',
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+
+  -- Crates automatic version completion
+  {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true
+          },
+        },
+      }
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+    end
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
