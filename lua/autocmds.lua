362e49d5ee0104f5471fa8510ad35718ddbb52b3
Minwoo Jeong <jeongmwj@gmail.com>
2025-12-21 22:05:59 +0900

    feat(style): add Linux kernel coding style

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -20,3 +20,31 @@ autocmd({ "BufWritePre" }, {
     vim.fn.setpos(".", save_cursor)
   end,
 })
+
+-- Linux Kernel Code Formatting
+autocmd("FileType", {
+  pattern = { "c", "cpp", "h", "make" },
+  callback = function()
+    -- CORRECT WAY: Look for "Kbuild" or "Kconfig" files in the current folder or parents
+    local get_root = vim.fs.find({ "Kbuild", "Kconfig" }, {
+      upward = true,
+      path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+    })
+
+    -- If we found a kernel build file, apply strict kernel rules
+    if #get_root > 0 then
+      vim.opt_local.tabstop = 8
+      vim.opt_local.shiftwidth = 8
+      vim.opt_local.softtabstop = 8
+      vim.opt_local.expandtab = false -- REAL TABS (Crucial!)
+      vim.opt_local.cindent = true
+      vim.opt_local.textwidth = 80
+      vim.opt_local.colorcolumn = "81"
+    else
+      -- Standard formatting for your other projects
+      vim.opt_local.tabstop = 4
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.expandtab = true
+    end
+  end,
+})
