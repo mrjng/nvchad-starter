521ecce21dceea6d5ab867e34590626eb544ce74
Minwoo Jeong <jeongmwj@gmail.com>
2025-12-21 21:36:08 +0900

    trim trailing whitespace on save automatically

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -8,3 +8,15 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto-trim trailing whitespace on save
+autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
