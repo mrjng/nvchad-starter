552802a68bb098333a6ca512f620a6d6487c0661
Minwoo Jeong <jeongmwj@gmail.com>
2026-01-20 08:53:03 +0900

    style: update comment and highlight colors

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,6 +1,6 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
@@ -9,10 +9,18 @@ M.base46 = {
 	theme = "tokyodark",
   transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- Override highlights here
+  hl_override = {
+    -- Change "CursorLine" to a darker/clearer background color
+    CursorLine = { bg = "#2c323c" },
+
+    -- Optional: If Visual selection is also hard to see, fix it here
+    Visual = { bg = "#3a404a" },
+
+    -- Make the comment italic
+    Comment = { italic = true , fg = "#8b9bb4"},
+	  ["@comment"] = { italic = true, fg = "#8b9bb4"},
+  },
 }
 
 -- M.nvdash = { load_on_startup = true }
